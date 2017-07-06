a5238dc0016af3bea67a1fa2f9c815caf0a2a12f
Patrick Haun <bomgar85@googlemail.com>
2017-07-06 19:07:48 +0200

    [feat] support multiple sources

--- src/main.rs
+++ src/main.rs
@@ -17,27 +17,30 @@ fn main() {
 }
 
 
-fn get_string_value_or_default(value: &Value, key: &str, default: &str) -> String {
-  match value.get(key) {
+fn get_string_value_or_default(value: &Value, keys: &[&str], default: &str) -> String {
+  let maybe_match = keys.iter().fold(None::<&Value>, | maybe_match, key | maybe_match.or(value.get(key)));
+  match maybe_match {
     Some(&Value::String(ref level)) => level.to_string(),
     _ => default.to_string()
   }
 }
 
 fn print_log_line(value: &Value) {
-  let level = get_string_value_or_default(value, "level", "unknown");
+  let level = get_string_value_or_default(value, &["level"], "unknown");
 
-  let formatted_level = format!("{:>6.6}", level);
+  let formatted_level = format!("{:>7.7}", level);
 
-  let colour = match level.as_ref() {
+  let colour = match level.to_lowercase().as_ref() {
     "info" => Colour::Green,
     "warn" => Colour::Yellow,
+    "warning" => Colour::Yellow,
     "error" => Colour::Red,
     "debug" => Colour::Blue,
     _ => Colour::Purple
   };
 
-  let message = get_string_value_or_default(value, "short_message", "");
+  let message = get_string_value_or_default(value, &["short_message", "msg"], "");
+  let timestamp =get_string_value_or_default(value, &["timestamp", "time"], "");
 
-  println!("{} | {}", colour.paint(formatted_level), message)
+  println!("{:>19.19} {} | {}", timestamp, colour.paint(formatted_level), message)
 }
