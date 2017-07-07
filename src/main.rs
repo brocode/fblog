d5523125920289b0f94c3499a38a77e44188bbe5
Patrick Haun <bomgar85@googlemail.com>
2017-07-07 11:40:04 +0200

    [refactor] cleanup code

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use ansi_term::{Colour, Style};
+use clap::{App, AppSettings, Arg};
 use serde_json::Value;
 use std::io::{self, BufRead};
-use clap::{App, Arg, AppSettings};
 
 #[macro_use]
 extern crate clap;
@@ -10,18 +10,11 @@ extern crate serde_json;
 extern crate ansi_term;
 
 fn main() {
-  let app = App::new("fblog")
-    .setting(AppSettings::ColoredHelp)
-    .version(crate_version!())
-    .author("Brocode inc <bros@brocode.sh>")
-    .about("json log viewer")
-    .arg(Arg::with_name("additional-value")
-           .short("a")
-           .multiple(true)
-           .takes_value(true)
-           .help("adds additional values"));
+  let app = app();
   let matches = app.get_matches();
-  let additional_values: Vec<&str> = matches.values_of("additional-value").map(|values| values.collect()).unwrap_or_default();
+  let additional_values: Vec<&str> = matches.values_of("additional-value")
+                                            .map(|values| values.collect())
+                                            .unwrap_or_default();
 
   let stdin = io::stdin();
   let reader = stdin.lock();
@@ -34,6 +27,18 @@ fn main() {
   }
 }
 
+fn app<'a>() -> App<'a, 'a> {
+  App::new("fblog")
+    .setting(AppSettings::ColoredHelp)
+    .version(crate_version!())
+    .author("Brocode inc <bros@brocode.sh>")
+    .about("json log viewer")
+    .arg(Arg::with_name("additional-value")
+           .short("a")
+           .multiple(true)
+           .takes_value(true)
+           .help("adds additional values"))
+}
 
 fn get_string_value(value: &Value, keys: &[&str]) -> Option<String> {
   let maybe_match = keys.iter()
@@ -50,21 +55,26 @@ fn get_string_value_or_default(value: &Value, keys: &[&str], default: &str) -> S
   get_string_value(value, keys).unwrap_or(default.to_string())
 }
 
+fn level_to_style(level: &str) -> Style {
+  match level.to_lowercase().as_ref() {
+    "info" => Colour::Green,
+    "warn" | "warning" => Colour::Yellow,
+    "error" | "err" => Colour::Red,
+    "debug" => Colour::Blue,
+    _ => Colour::Purple,
+  }
+  .bold()
+}
+
 fn print_log_line(value: &Value, additional_values: &[&str]) {
   let bold = Style::new().bold();
   let bold_grey = Colour::RGB(150, 150, 150).bold();
+
   let level = get_string_value_or_default(value, &["level", "severity"], "unknown");
 
   let formatted_level = format!("{:>7.7}", level.to_uppercase());
 
-  let colour = match level.to_lowercase().as_ref() {
-    "info" => Colour::Green,
-    "warn" => Colour::Yellow,
-    "warning" => Colour::Yellow,
-    "error" => Colour::Red,
-    "debug" => Colour::Blue,
-    _ => Colour::Purple,
-  }.bold();
+  let level_style = level_to_style(&level);
 
   let message = get_string_value_or_default(value, &["short_message", "msg"], "");
   let timestamp = get_string_value_or_default(value, &["timestamp", "time"], "");
@@ -72,10 +82,10 @@ fn print_log_line(value: &Value, additional_values: &[&str]) {
 
   println!("{} {} - {}",
            painted_timestamp,
-           colour.paint(formatted_level),
+           level_style.paint(formatted_level),
            message);
   for additional_value in additional_values {
-    if let Some(value) = get_string_value(value, &[additional_value]){
+    if let Some(value) = get_string_value(value, &[additional_value]) {
       let trimmed_additional_value = format!("@{:<10.10}", additional_value.to_string());
       let painted_value = bold_grey.paint(trimmed_additional_value);
       println!("                {}   {}", painted_value, value);
