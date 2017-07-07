e078c8b67eb72f7ebc4f210ba064d56b895dcce6
Patrick Haun <bomgar85@googlemail.com>
2017-07-07 17:32:07 +0200

    [refactor] cleanup code

---
+++ src/log.rs
@@ -0,0 +1,60 @@
+use ansi_term::{Colour, Style};
+use std::collections::BTreeMap;
+
+fn get_string_value(value: &BTreeMap<String, String>, keys: &[&str]) -> Option<String> {
+  keys.iter()
+      .fold(None::<String>,
+            |maybe_match, key| maybe_match.or_else(|| value.get(*key).map(|k| k.to_owned())))
+}
+
+fn get_string_value_or_default(value: &BTreeMap<String, String>, keys: &[&str], default: &str) -> String {
+  get_string_value(value, keys).unwrap_or_else(|| default.to_string())
+}
+
+fn level_to_style(level: &str) -> Style {
+  match level.to_lowercase().as_ref() {
+    "info" => Colour::Green,
+    "warn" | "warning" => Colour::Yellow,
+    "error" | "err" => Colour::Red,
+    "debug" => Colour::Blue,
+    _ => Colour::Purple,
+  }
+  .bold()
+}
+
+
+pub fn print_log_line(log_entry: &BTreeMap<String, String>, additional_values: &[String], dump_all: bool) {
+  let bold = Style::new().bold();
+
+  let level = get_string_value_or_default(log_entry, &["level", "severity"], "unknown");
+
+  let formatted_level = format!("{:>5.5}:", level.to_uppercase());
+
+  let level_style = level_to_style(&level);
+
+  let message = get_string_value_or_default(log_entry, &["short_message", "msg", "message"], "");
+  let timestamp = get_string_value_or_default(log_entry, &["timestamp", "time"], "");
+  let painted_timestamp = bold.paint(format!("{:>19.19}", timestamp));
+
+  println!("{} {} {}",
+           painted_timestamp,
+           level_style.paint(formatted_level),
+           message);
+  if dump_all {
+    let all_values: Vec<String> = log_entry.keys().map(|k| k.to_owned()).collect();
+    write_additional_values(log_entry, all_values.as_slice());
+  } else {
+    write_additional_values(log_entry, additional_values);
+  }
+}
+
+fn write_additional_values(log_entry: &BTreeMap<String, String>, additional_values: &[String]) {
+  let bold_grey = Colour::RGB(150, 150, 150).bold();
+  for additional_value in additional_values {
+    if let Some(value) = get_string_value(log_entry, &[additional_value]) {
+      let trimmed_additional_value = format!("{:>25.25}:", additional_value.to_string());
+      let painted_value = bold_grey.paint(trimmed_additional_value);
+      println!("{} {}", painted_value, value);
+    }
+  }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use ansi_term::{Colour, Style};
 use clap::{App, AppSettings, Arg};
-use serde_json::{Value, Map};
+use serde_json::{Map, Value};
+use std::collections::BTreeMap;
 use std::io::{self, BufRead};
 
 #[macro_use]
@@ -9,12 +9,14 @@ extern crate clap;
 extern crate serde_json;
 extern crate ansi_term;
 
+mod log;
+
 fn main() {
   let app = app();
   let matches = app.get_matches();
   let additional_values: Vec<String> = matches.values_of("additional-value")
-                                            .map(|values| values.map(|v| v.to_owned()).collect())
-                                            .unwrap_or_default();
+                                              .map(|values| values.map(|v| v.to_owned()).collect())
+                                              .unwrap_or_default();
 
   let dump_all = matches.is_present("dump-all");
 
@@ -23,7 +25,7 @@ fn main() {
   for line in reader.lines() {
     let read_line = &line.expect("Should be able to read line");
     match serde_json::from_str::<Value>(read_line) {
-      Ok(Value::Object(log_entry)) => print_log_line(&log_entry, &additional_values, dump_all),
+      Ok(Value::Object(log_entry)) => log::print_log_line(&extract_string_values(&log_entry), &additional_values, dump_all),
       _ => println!("??? > {}", read_line),
     };
   }
@@ -47,64 +49,13 @@ fn app<'a>() -> App<'a, 'a> {
            .help("dumps all values"))
 }
 
-fn get_string_value(value: &Map<String, Value>, keys: &[&str]) -> Option<String> {
-  let maybe_match = keys.iter()
-                        .fold(None::<&Value>, |maybe_match, key| {
-    maybe_match.or_else(|| value.get(*key))
-  });
-  match maybe_match {
-    Some(&Value::String(ref level)) => Some(level.to_string()),
-    _ => None,
-  }
-}
-
-fn get_string_value_or_default(value: &Map<String, Value>, keys: &[&str], default: &str) -> String {
-  get_string_value(value, keys).unwrap_or_else(|| default.to_string())
-}
-
-fn level_to_style(level: &str) -> Style {
-  match level.to_lowercase().as_ref() {
-    "info" => Colour::Green,
-    "warn" | "warning" => Colour::Yellow,
-    "error" | "err" => Colour::Red,
-    "debug" => Colour::Blue,
-    _ => Colour::Purple,
-  }
-  .bold()
-}
-
-fn print_log_line(log_entry: &Map<String, Value>, additional_values: &[String], dump_all: bool) {
-  let bold = Style::new().bold();
-
-  let level = get_string_value_or_default(log_entry, &["level", "severity"], "unknown");
-
-  let formatted_level = format!("{:>5.5}:", level.to_uppercase());
-
-  let level_style = level_to_style(&level);
-
-  let message = get_string_value_or_default(log_entry, &["short_message", "msg", "message"], "");
-  let timestamp = get_string_value_or_default(log_entry, &["timestamp", "time"], "");
-  let painted_timestamp = bold.paint(format!("{:>19.19}", timestamp));
-
-  println!("{} {} {}",
-           painted_timestamp,
-           level_style.paint(formatted_level),
-           message);
-  if dump_all {
-    let all_values: Vec<String> = log_entry.keys().map(|k| k.to_owned()).collect();
-    write_additional_values(log_entry, all_values.as_slice());
-  } else {
-    write_additional_values(log_entry, additional_values);
-  }
-}
-
-fn write_additional_values(log_entry: &Map<String, Value>, additional_values: &[String]) {
-  let bold_grey = Colour::RGB(150, 150, 150).bold();
-  for additional_value in additional_values {
-    if let Some(value) = get_string_value(log_entry, &[additional_value]) {
-      let trimmed_additional_value = format!("{:>25.25}:", additional_value.to_string());
-      let painted_value = bold_grey.paint(trimmed_additional_value);
-      println!("{} {}", painted_value, value);
+fn extract_string_values(log_entry: &Map<String, Value>) -> BTreeMap<String, String> {
+  let mut string_map: BTreeMap<String, String> = BTreeMap::new();
+  for (key, value) in log_entry {
+    if let &Value::String(ref string_value) = value {
+      string_map.insert(key.to_owned(), string_value.to_owned());
     }
   }
+  string_map
 }
+
