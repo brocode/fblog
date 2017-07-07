bb9a6a5192745e9a92652dfee3cdd5f74bc76ff4
Patrick Haun <bomgar85@googlemail.com>
2017-07-07 08:57:07 +0200

    [refactor] reformat code

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,31 @@
-use std::io::{self, BufRead};
-use serde_json::Value;
 use ansi_term::Colour;
+use serde_json::Value;
+use std::io::{self, BufRead};
 
 extern crate serde_json;
 extern crate ansi_term;
 
 fn main() {
-   let stdin = io::stdin();
+  let stdin = io::stdin();
   let reader = stdin.lock();
   for line in reader.lines() {
-    match serde_json::from_str::<Value>(&line.expect("Should be able to read line")) {
+    let read_line = &line.expect("Should be able to read line");
+    match serde_json::from_str::<Value>(&read_line) {
       Ok(value) => print_log_line(&value),
-      Err(_) => println!("fb: Unparseable line")
+      Err(_) => println!("??? > {}", read_line),
     };
   }
 }
 
 
 fn get_string_value_or_default(value: &Value, keys: &[&str], default: &str) -> String {
-  let maybe_match = keys.iter().fold(None::<&Value>, | maybe_match, key | maybe_match.or(value.get(key)));
+  let maybe_match = keys.iter()
+                        .fold(None::<&Value>, |maybe_match, key| {
+    maybe_match.or(value.get(key))
+  });
   match maybe_match {
     Some(&Value::String(ref level)) => level.to_string(),
-    _ => default.to_string()
+    _ => default.to_string(),
   }
 }
 
@@ -36,11 +40,14 @@ fn print_log_line(value: &Value) {
     "warning" => Colour::Yellow,
     "error" => Colour::Red,
     "debug" => Colour::Blue,
-    _ => Colour::Purple
+    _ => Colour::Purple,
   };
 
   let message = get_string_value_or_default(value, &["short_message", "msg"], "");
-  let timestamp =get_string_value_or_default(value, &["timestamp", "time"], "");
+  let timestamp = get_string_value_or_default(value, &["timestamp", "time"], "");
 
-  println!("{:>19.19} {} | {}", timestamp, colour.paint(formatted_level), message)
+  println!("{:>19.19} {} | {}",
+           timestamp,
+           colour.paint(formatted_level),
+           message)
 }
