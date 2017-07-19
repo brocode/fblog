a387f67e4ecde5e1f0bb8e2fb51159ab4e1f8940
Patrick Haun <bomgar85@googlemail.com>
2017-07-19 19:47:44 +0200

    [build] static binary

--- package.sh
+++ package.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 set -x -e -o pipefail
 
-cargo build --release
+rm -rf target
+docker run --rm -it -v "$(pwd)":/home/rust/src ekidd/rust-musl-builder:1.18.0 cargo build --release
 name=fblog
-version=$(target/release/$name --version | cut -d ' ' -f 2)
+target_dir="target/x86_64-unknown-linux-musl/release"
 
-rm -f target/release/$name.rpm
-docker run -v $(readlink -e ./target/release):/release -it --rm  alanfranz/fpm-within-docker:centos-7     fpm -s dir -t rpm -n $name -p /release/$name.rpm -v $version /release/$name=/usr/bin/$name
+version=$($target_dir/$name --version | cut -d ' ' -f 2)
+
+docker run -v $(readlink -e ./$target_dir):/release -it --rm  alanfranz/fpm-within-docker:centos-7     fpm -s dir -t rpm -n $name -p /release/$name.rpm -v $version /release/$name=/usr/bin/$name
 rm -f target/release/$name.deb
-docker run -v $(readlink -e ./target/release):/release -it --rm  alanfranz/fpm-within-docker:ubuntu-zesty fpm -s dir -t deb -n $name -p /release/$name.deb -v $version /release/$name=/usr/bin/$name
+docker run -v $(readlink -e ./$target_dir):/release -it --rm  alanfranz/fpm-within-docker:ubuntu-zesty fpm -s dir -t deb -n $name -p /release/$name.deb -v $version /release/$name=/usr/bin/$name
 
