04f393611ccf2c11498d5678ba7287d4f5722b44
Patrick Haun <bomgar85@googlemail.com>
2017-07-20 07:55:06 +0200

    [cleanup] useless rm

--- package.sh
+++ package.sh
@@ -9,6 +9,5 @@ target_dir="target/x86_64-unknown-linux-musl/release"
 version=$($target_dir/$name --version | cut -d ' ' -f 2)
 
 docker run -v $(readlink -e ./$target_dir):/release -it --rm  alanfranz/fpm-within-docker:centos-7     fpm -s dir -t rpm -n $name -p /release/$name.rpm -v $version /release/$name=/usr/bin/$name
-rm -f target/release/$name.deb
 docker run -v $(readlink -e ./$target_dir):/release -it --rm  alanfranz/fpm-within-docker:ubuntu-zesty fpm -s dir -t deb -n $name -p /release/$name.deb -v $version /release/$name=/usr/bin/$name
 
