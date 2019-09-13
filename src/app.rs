7e76e166358f6c45f743aa34c5dd5b00c984c008
Patrick Haun <patrick@bomgar.email>
2019-09-13 11:59:06 +0200

    [refactor] app module

---
+++ src/app.rs
@@ -0,0 +1,91 @@
+use clap::{App, AppSettings, Arg};
+use clap::crate_version;
+
+pub fn app<'a>() -> App<'a, 'a> {
+  App::new("fblog")
+    .global_setting(AppSettings::ColoredHelp)
+    .version(crate_version!())
+    .author("Brocode inc <bros@brocode.sh>")
+    .about("json log viewer")
+    .arg(
+      Arg::with_name("additional-value")
+        .long("additional-value")
+        .short("a")
+        .multiple(true)
+        .number_of_values(1)
+        .takes_value(true)
+        .help("adds additional values"),
+    )
+    .arg(
+      Arg::with_name("message-key")
+        .long("message-key")
+        .short("m")
+        .multiple(true)
+        .number_of_values(1)
+        .takes_value(true)
+        .help("Adds an additional key to detect the message in the log entry."),
+    )
+    .arg(
+      Arg::with_name("time-key")
+        .long("time-key")
+        .short("t")
+        .multiple(true)
+        .number_of_values(1)
+        .takes_value(true)
+        .help("Adds an additional key to detect the time in the log entry."),
+    )
+    .arg(
+      Arg::with_name("level-key")
+        .long("level-key")
+        .short("l")
+        .multiple(true)
+        .number_of_values(1)
+        .takes_value(true)
+        .help("Adds an additional key to detect the level in the log entry."),
+    )
+    .arg(
+      Arg::with_name("dump-all")
+        .long("dump-all")
+        .short("d")
+        .multiple(false)
+        .takes_value(false)
+        .help("dumps all values"),
+    )
+    .arg(
+      Arg::with_name("with-prefix")
+        .long("with-prefix")
+        .short("p")
+        .multiple(false)
+        .takes_value(false)
+        .help("consider all text before opening curly brace as prefix"),
+    )
+    .arg(
+      Arg::with_name("filter")
+        .long("filter")
+        .short("f")
+        .multiple(false)
+        .takes_value(true)
+        .help("lua expression to filter log entries. `message ~= nil and string.find(message, \"text.*\") ~= nil`"),
+    )
+    .arg(
+      Arg::with_name("no-implicit-filter-return-statement")
+        .long("no-implicit-filter-return-statement")
+        .multiple(false)
+        .takes_value(false)
+        .help("if you pass a filter expression 'return' is automatically prepended. Pass this switch to disable the implicit return."),
+    )
+    .arg(
+      Arg::with_name("INPUT")
+        .help("Sets the input file to use, otherwise assumes stdin")
+        .required(false)
+        .default_value("-"),
+    )
+    .arg(
+      Arg::with_name("inspect")
+        .long("inspect")
+        .short("i")
+        .multiple(false)
+        .takes_value(false)
+        .help("only prints json keys not encountered before"),
+    )
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use ansi_term::Colour;
-use clap::{App, AppSettings, Arg};
 use serde_json::{Map, Value};
 use std::collections::BTreeMap;
 use std::fs;
@@ -12,17 +11,17 @@ extern crate maplit;
 #[cfg(test)]
 extern crate regex;
 
-use clap::crate_version;
 
 mod filter;
 mod inspect;
 mod log;
+mod app;
 
 use crate::inspect::InspectLogger;
 use crate::log::LogSettings;
 
 fn main() {
-  let app = app();
+  let app = app::app();
   let matches = app.get_matches();
 
   let mut log_settings = LogSettings::new_default_settings();
@@ -144,94 +143,7 @@ fn process_log_entry(log_settings: &LogSettings, inspect_logger: &mut InspectLog
   }
 }
 
-fn app<'a>() -> App<'a, 'a> {
-  App::new("fblog")
-    .global_setting(AppSettings::ColoredHelp)
-    .version(crate_version!())
-    .author("Brocode inc <bros@brocode.sh>")
-    .about("json log viewer")
-    .arg(
-      Arg::with_name("additional-value")
-        .long("additional-value")
-        .short("a")
-        .multiple(true)
-        .number_of_values(1)
-        .takes_value(true)
-        .help("adds additional values"),
-    )
-    .arg(
-      Arg::with_name("message-key")
-        .long("message-key")
-        .short("m")
-        .multiple(true)
-        .number_of_values(1)
-        .takes_value(true)
-        .help("Adds an additional key to detect the message in the log entry."),
-    )
-    .arg(
-      Arg::with_name("time-key")
-        .long("time-key")
-        .short("t")
-        .multiple(true)
-        .number_of_values(1)
-        .takes_value(true)
-        .help("Adds an additional key to detect the time in the log entry."),
-    )
-    .arg(
-      Arg::with_name("level-key")
-        .long("level-key")
-        .short("l")
-        .multiple(true)
-        .number_of_values(1)
-        .takes_value(true)
-        .help("Adds an additional key to detect the level in the log entry."),
-    )
-    .arg(
-      Arg::with_name("dump-all")
-        .long("dump-all")
-        .short("d")
-        .multiple(false)
-        .takes_value(false)
-        .help("dumps all values"),
-    )
-    .arg(
-      Arg::with_name("with-prefix")
-        .long("with-prefix")
-        .short("p")
-        .multiple(false)
-        .takes_value(false)
-        .help("consider all text before opening curly brace as prefix"),
-    )
-    .arg(
-      Arg::with_name("filter")
-        .long("filter")
-        .short("f")
-        .multiple(false)
-        .takes_value(true)
-        .help("lua expression to filter log entries. `message ~= nil and string.find(message, \"text.*\") ~= nil`"),
-    )
-    .arg(
-      Arg::with_name("no-implicit-filter-return-statement")
-        .long("no-implicit-filter-return-statement")
-        .multiple(false)
-        .takes_value(false)
-        .help("if you pass a filter expression 'return' is automatically prepended. Pass this switch to disable the implicit return."),
-    )
-    .arg(
-      Arg::with_name("INPUT")
-        .help("Sets the input file to use, otherwise assumes stdin")
-        .required(false)
-        .default_value("-"),
-    )
-    .arg(
-      Arg::with_name("inspect")
-        .long("inspect")
-        .short("i")
-        .multiple(false)
-        .takes_value(false)
-        .help("only prints json keys not encountered before"),
-    )
-}
+
 
 fn extract_string_values(log_entry: &Map<String, Value>) -> BTreeMap<String, String> {
   let mut string_map: BTreeMap<String, String> = BTreeMap::new();
