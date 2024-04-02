00cd2b1fe97a79ce685a2ad9e24fb612307799e3
Patrick Haun <patrick@bomgar.email>
2024-04-02 20:44:19 +0200

    yansi update

--- src/template.rs
+++ src/template.rs
@@ -1,57 +1,42 @@
-use crate::no_color_support::{paint, style};
 use handlebars::{handlebars_helper, no_escape, Handlebars};
 use std::convert::TryInto;
-use yansi::{Color, Style};
+use yansi::{Color, Paint};
 
 pub static DEFAULT_MAIN_LINE_FORMAT: &str =
     "{{bold(fixed_size 19 fblog_timestamp)}} {{level_style (uppercase (fixed_size 5 fblog_level))}}:{{#if fblog_prefix}} {{bold(cyan fblog_prefix)}}{{/if}} {{fblog_message}}";
 pub static DEFAULT_ADDITIONAL_VALUE_FORMAT: &str = "{{bold (color_rgb 150 150 150 (fixed_size 25 key))}}: {{value}}";
 
-fn level_to_style(level: &str) -> Style {
-    match level.trim().to_lowercase().as_ref() {
-        "trace" => Color::Cyan,
-        "debug" => Color::Blue,
-        "info" => Color::Green,
-        "warn" | "warning" => Color::Yellow,
-        "error" | "err" => Color::Red,
-        "fatal" => Color::Magenta,
-        _ => Color::Magenta,
-    }
-    .style()
-    .bold()
-}
-
 pub fn fblog_handlebar_registry(main_line_format: String, additional_value_format: String) -> Handlebars<'static> {
     handlebars_helper!(bold: |t: str| {
-        style(&Color::Default.style().bold(), t)
+        format!("{}", t.bold())
     });
 
     handlebars_helper!(cyan: |t: str| {
-        paint(Color::Cyan, t)
+        format!("{}", t.cyan())
     });
 
     handlebars_helper!(yellow: |t: str| {
-        paint(Color::Yellow, t)
+        format!("{}", t.yellow())
     });
 
     handlebars_helper!(red: |t: str| {
-        paint(Color::Red, t)
+        format!("{}", t.red())
     });
 
     handlebars_helper!(blue: |t: str| {
-        paint(Color::Blue, t)
+        format!("{}", t.blue())
     });
 
     handlebars_helper!(purple: |t: str| {
-        paint(Color::Magenta, t)
+        format!("{}", t.magenta())
     });
 
     handlebars_helper!(green: |t: str| {
-        paint(Color::Green, t)
+        format!("{}", t.green())
     });
 
     handlebars_helper!(color_rgb: |r: u64, g: u64, b: u64, t: str| {
-        paint(Color::RGB(r.try_into().unwrap(), g.try_into().unwrap(), b.try_into().unwrap()), t)
+        format!("{}", t.rgb(r.try_into().unwrap(), g.try_into().unwrap(), b.try_into().unwrap()))
     });
 
     handlebars_helper!(uppercase: |t: str| {
@@ -59,8 +44,16 @@ pub fn fblog_handlebar_registry(main_line_format: String, additional_value_forma
     });
 
     handlebars_helper!(level_style: |level: str| {
-        let s = level_to_style(level);
-        style(&s, level)
+        let color = match level.trim().to_lowercase().as_ref() {
+            "trace" => Color::Cyan,
+            "debug" => Color::Blue,
+            "info" => Color::Green,
+            "warn" | "warning" => Color::Yellow,
+            "error" | "err" => Color::Red,
+            "fatal" => Color::Magenta,
+            _ => Color::Magenta,
+        };
+        format!("{}", level.fg(color).bold())
     });
 
     handlebars_helper!(fixed_size: |isize: u64, t: str| {
